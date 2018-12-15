7edbe6ed8e529cda922c3bf545fa9a3eed0f744a
andresvr <avrz237@gmail.com>
2018-12-14 23:38:47 -0500

    add reservation model

--- tech_mornings/reservation/models.py
+++ tech_mornings/reservation/models.py
@@ -19,3 +19,27 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__ (self):
         return self.choice_text
+
+class Client(models.Model):
+    identity_card = models.CharField(max_length = 13)
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length = 50)
+    e_mail = models.CharField(max_length = 200)
+    create_date = models.DateTimeField('create at')
+    modify_date = models.DateTimeField('modify at')
+
+    def __str__ (self):
+        return self.cli_name
+
+    def was_create_recently(self):
+        return self.create_date >= timezone.now()-datetime.timedelta(days=1)
+        
+class Reservation(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    reservation_date = models.DateTimeField('reservation date')
+    people_quatity = models.IntegerField(default=1)
+    menu_type = models.CharField(max_length=50)
+    payment_method = models.CharField(max_length=50)
+
+    def __str__ (self):
+        return self.reservation_date + self.people_quatity         
