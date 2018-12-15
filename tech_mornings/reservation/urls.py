403b19e2ece6a48d0a1d2580b449169f0ba05053
andresvr <avrz237@gmail.com>
2018-12-15 02:33:26 -0500

    add some comentaries from code

--- tech_mornings/reservation/urls.py
+++ tech_mornings/reservation/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name ='index'),
-    path('new/', views.ReservationCreate.as_view() , name ='reservation-add'),
-    path('list/', views.ReservationList.as_view() , name ='reservation-view')
+    path('', views.index, name ='index'), # this path is not a class because you need to define a single code for sorting results
+    path('new/', views.ReservationCreate.as_view() , name ='reservation-add'), # this path is created based on create view django tool
+    path('list/', views.ReservationList.as_view() , name ='reservation-view') # this path is created based on list view django tool
 
 ]
--- tech_mornings/reservation/views.py
+++ tech_mornings/reservation/views.py
@@ -4,7 +4,14 @@ from .models import Client, Reservation
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 
+
 def index (request):
+    """
+    this method alows to create a shorting by list from clients
+    and use render object to present into view.
+    :param request: request.
+    :return: render object instance.
+    """
     clients_list = Client.objects.order_by('-create_date')
     context = {
         'clients_list': clients_list
@@ -12,10 +19,16 @@ def index (request):
     return render(request, 'reservation/index.html',context)
 
 class ReservationCreate(CreateView):
+    """
+    this class allows to create reservation view.
+    """
     model = Reservation
     success_url = '/reservation/list'
     fields = '__all__'
 
 class ReservationList(ListView):
+    """
+    this class allows to create reservation list view.
+    """
     model = Reservation
 
